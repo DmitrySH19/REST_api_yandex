49982840d3e6097e80cbe8e7847d7f9b76d6508f
ezrial321 <shirocov.dmitry@gmail.com>
2021-03-29 03:03:22 -0700

    uwsgi

--- courier_statistic.py
+++ courier_statistic.py
@@ -1,10 +1,11 @@
 import dateutil.parser
 import numpy as np
 
+
 class courier_statistic():
     def __init__(self, courier, complite_orders):
         self.courier = courier
-        self.complite_orders = sorted(complite_orders,key = lambda x: x[1])
+        self.complite_orders = sorted(complite_orders, key=lambda x: x[1])
         self.count_orders = len(self.complite_orders)
 
     def calulate_statistic(self):
@@ -16,10 +17,10 @@ class courier_statistic():
         # print(self.count_orders)
         rating = self.rating(mean_time)
         earnings = self.earnings()
-        return (rating,earnings)
+        return (rating, earnings)
 
-    def rating (self, mean_time):
-        return ((60*60 - min(mean_time,60*60))/(60*60)) *5
+    def rating(self, mean_time):
+        return ((60*60 - min(mean_time, 60*60))/(60*60)) * 5
 
     def earnings(self):
         koef = 2
@@ -31,11 +32,11 @@ class courier_statistic():
 
     def split_by_region(self):
         cur_region = self.complite_orders[0][1]
-        orderered_orders  = [[]]
+        orderered_orders = [[]]
         counter = 0
         for order in self.complite_orders:
             if cur_region == order[1]:
-                
+
                 orderered_orders[counter].append(order)
             else:
                 orderered_orders.append([order])
@@ -44,26 +45,25 @@ class courier_statistic():
         self.complite_orders = orderered_orders
 
     def sort_in_order(self):
-        print('before',self.complite_orders)
+        print('before', self.complite_orders)
         sorted_orders = []
         for ord in self.complite_orders:
-            sorted_orders.append(sorted(ord,key= lambda x: x[4]))
+            sorted_orders.append(sorted(ord, key=lambda x: x[4]))
 
         self.complite_orders = sorted_orders
-        print('after',self.complite_orders)
+        print('after', self.complite_orders)
 
     def find_min_time(self):
-        mean_times  = []
+        mean_times = []
         for region in self.complite_orders:
             mean_times.append(self.calculate_mean_times(region))
-        print('mean_times',mean_times)
+        print('mean_times', mean_times)
         mean_per_region = list(map(np.mean, mean_times))
-        print('mean_times',mean_per_region)
+        print('mean_times', mean_per_region)
         min_mean_time = min(mean_per_region)
         print('min_time', min_mean_time)
         return min_mean_time
 
-    
     def calculate_mean_times(self, region):
         prev_time = None
         mean_times = []
@@ -74,10 +74,10 @@ class courier_statistic():
                 prev_time = complite
                 mean_times.append(delta.seconds)
             else:
-                assign, complite = dateutil.parser.isoparse(ord[2]),dateutil.parser.isoparse(ord[3])
+                assign, complite = dateutil.parser.isoparse(
+                    ord[2]), dateutil.parser.isoparse(ord[3])
                 delta = abs(assign - complite)
                 mean_times.append(delta.seconds)
                 prev_time = complite
-        
-        return mean_times
 
+        return mean_times
--- sql_alc.py
+++ sql_alc.py
@@ -3,18 +3,20 @@ from sqlalchemy.orm import sessionmaker
 from courier_model import courier_model, formatted_date
 from courier_statistic import courier_statistic
 
-#class for connect database and flask requests
+# class for connect database and flask requests
+
+
 class sqlite_connector():
     def __init__(self, db_obj):
         self.db = db_obj
 
-    def insert_couers(self,json_response):
+    def insert_couers(self, json_response):
         insert_id = []
         couriers = json_response['data']
         session = self.db.load_sesion()
 
         for courier in couriers:
-            self.db.insert_courier(courier,session)
+            self.db.insert_courier(courier, session)
             insert_id.append({'id': courier['courier_id']})
 
         session.commit()
@@ -24,31 +26,32 @@ class sqlite_connector():
 
     def patch_courier(self, json_response, courier_id):
         session = self.db.load_sesion()
-        edited = self.db.valid_id_courier(courier_id,session)
+        edited = self.db.valid_id_courier(courier_id, session)
         if edited:
             for key in json_response.keys():
-                edited = self.db.courier_patch_by_key(courier_id, {key:json_response[key]}, session)
-        if self.db.courier_is_in_complete_orders(edited['courier_id'],session):
-            uncomplite_ids = [x[0] for x in self.db.get_uncomplete_orders(edited['courier_id'],session)]
+                edited = self.db.courier_patch_by_key(
+                    courier_id, {key: json_response[key]}, session)
+        if self.db.courier_is_in_complete_orders(edited['courier_id'], session):
+            uncomplite_ids = [x[0] for x in self.db.get_uncomplete_orders(
+                edited['courier_id'], session)]
             check_orders = []
             for ord in uncomplite_ids:
-                check_orders.append(self.db.get_order_by_id(ord,session))
-              
-            check_output = courier_model(edited,check_orders).assign_orders()
+                check_orders.append(self.db.get_order_by_id(ord, session))
+
+            check_output = courier_model(edited, check_orders).assign_orders()
             check_orders = [x['order'] for x in check_orders]
             #check_output = [x['order_id'] for x in check_output]
-            print('change_cour',edited)
+            print('change_cour', edited)
             if check_orders != check_output:
                 print('change')
                 for ord in check_orders:
                     print(ord)
-                    self.db.delete_uncomplete_by_id(ord,session)
-                self.db.insert_into_complete(edited,check_output,session)
-                
-            print("1",check_orders)
-            print("2",check_output)
+                    self.db.delete_uncomplete_by_id(ord, session)
+                self.db.insert_into_complete(edited, check_output, session)
 
-        
+            print("1", check_orders)
+            print("2", check_output)
+
         session.commit()
         session.close()
         return edited
@@ -65,58 +68,64 @@ class sqlite_connector():
         session.close()
         return insert_id
 
-    def assign_orders(self, json_response): 
+    def assign_orders(self, json_response):
         session = self.db.load_sesion()
-        if self.db.valid_id_courier(json_response['courier_id'],session):
+        if self.db.valid_id_courier(json_response['courier_id'], session):
             # filter orders by region and weight
-            uncomplete = self.db.get_uncomplete_orders(json_response['courier_id'],session)    
-            print('UNCOMPLETE',uncomplete)
+            uncomplete = self.db.get_uncomplete_orders(
+                json_response['courier_id'], session)
+            print('UNCOMPLETE', uncomplete)
             if uncomplete:
                 answer = {
-                        'orders':[{'id':x[0]} for x in uncomplete],
-                        "assign_time": uncomplete[0][1]
-                        }
+                    'orders': [{'id': x[0]} for x in uncomplete],
+                    "assign_time": uncomplete[0][1]
+                }
 
                 session.commit()
                 session.close()
                 return answer
             else:
-                available_orders = self.db.order_filtered(json_response['courier_id'], session)
-                
-                courier = self.db.get_couier_by_id(json_response['courier_id'],session)
-                answer = {'orders':[]}
+                available_orders = self.db.order_filtered(
+                    json_response['courier_id'], session)
+
+                courier = self.db.get_couier_by_id(
+                    json_response['courier_id'], session)
+                answer = {'orders': []}
                 if available_orders:
 
-                    take_ids = courier_model(courier=courier,orders = available_orders).assign_orders()
-                    answer = self.db.insert_into_complete(courier,take_ids,session)
+                    take_ids = courier_model(
+                        courier=courier, orders=available_orders).assign_orders()
+                    answer = self.db.insert_into_complete(
+                        courier, take_ids, session)
 
                     session.commit()
                     session.close()
-                return answer                       
+                return answer
         else:
             return False
-    
-    def complete_orders(self,json_request):
+
+    def complete_orders(self, json_request):
 
         session = self.db.load_sesion()
-        if self.db.is_in_complete_orders(json_request['order_id'],session)\
-        and self.db.courier_is_in_complete_orders(json_request['courier_id'], session):
-            answer = self.db.inset_complete_time(json_request['order_id'],json_request['courier_id'],\
-                                        json_request['complete_time'],session)
+        if self.db.is_in_complete_orders(json_request['order_id'], session)\
+                and self.db.courier_is_in_complete_orders(json_request['courier_id'], session):
+            answer = self.db.inset_complete_time(json_request['order_id'], json_request['courier_id'],
+                                                 json_request['complete_time'], session)
             session.commit()
             session.close()
             return answer
         else:
             return False
-                                          
+
     def get_courier(self, courier_id):
         session = self.db.load_sesion()
-        if self.db.valid_id_courier(courier_id,session):
-            courier = self.db.get_couier_by_id(courier_id,session)
-            if self.db.is_courier_complete_orders(courier_id,session):
-                orders = self.db.get_complete_orders(courier_id,session)
-                rating, earnings =courier_statistic(courier,orders).calulate_statistic()
-                print('rating',rating)
+        if self.db.valid_id_courier(courier_id, session):
+            courier = self.db.get_couier_by_id(courier_id, session)
+            if self.db.is_courier_complete_orders(courier_id, session):
+                orders = self.db.get_complete_orders(courier_id, session)
+                rating, earnings = courier_statistic(
+                    courier, orders).calulate_statistic()
+                print('rating', rating)
                 print('earnings', earnings)
                 session.commit()
                 session.close()
